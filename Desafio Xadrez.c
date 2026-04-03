d9d788e54957b97d6f659170fd66c0420721d210
eliseumc1999-ui <eliseumc1999@gmail.com>
2026-04-03 02:02:09 +0000

    Nivel aventureiro

--- Desafio Xadrez.c
+++ Desafio Xadrez.c
@@ -1,23 +1,54 @@
 #include <stdio.h>
 
 int main (){
+int i = 0;
+int bispo, torre, rainha, cavalo = 1;
 
-  // ESTRUTURA FOR - MOVIMENTOS DA TORRE 
+    // Entrada da quantidade de movimentos do Bispo
+    printf("Número de movimentos do bispo:"); 
+    scanf("%d", &bispo);
 
-     int numero, i;
+    //Estrutura Movimentos do Bispo
+    while (i < bispo) 
+    {
+        printf("Cima Direita\n",i); // imprime os movimentos do Bispo
+        i++;
+    }
 
-    // Entrada da quantidade de movimentos da torre
-    printf("Escolha o numero de movimentos:");  
-    scanf("%d", &numero); 
+    // Entrada da quantidade de movimentos da Torre
+    printf("\nEscolha o numero de movimentos da torre:");  
+    scanf("%d", &torre); 
 
         
-    // Mover torre 5 casas a direita
-    for (int i = 0; i < numero; i++){ 
+    // Estrutura Movimentos da Torre
+    for (int i = 0; i < torre; i++){ 
     
-        printf("Direita \n", i); // imprime o movimento da torre
+        printf("Direita \n", i); // imprime o movimento da Torre
     }
   
-  return 0;
+    // Entrada de dados de movimento da Rainha
+    printf("\nEscolha o numero de movimentos da rainha:");
+    scanf("%d",&rainha);
 
+    // Estrutura Movimentos da Rainha
+    for ( i = 0; i < rainha; i++) 
+    {
+      printf("Esquerda \n",i);  //imprime o movimento da Rainha
+    }
+
+    // Estrutura Movimentos do Cavalo
+    printf("\nMovimento do Cavalo: \n");
+
+    while (cavalo--)
+  {
+    for (int i = 0; i < 2; i++)
+    {
+        printf("cima \n"); // imprime os movimentos do Cavalo
+    }
+    
+        printf("direita \n"); // imprime os movimentos do Cavalo
+  }
+  
+  return 0;
 
 }
